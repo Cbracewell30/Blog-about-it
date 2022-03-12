ea8dea26b31866f831668cff1090cd4224f2f78f
Chris <chrisbracewell35@gmail.com>
2022-03-12 00:09:12 -0500

    adding user model

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require("sequelize");
+// password hashing
+const bcrypt = require("bcrypt");
+const { type } = require("os");
+const { before } = require("lodash");
+
+// Creating User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// Creating the users fields and columns
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  {
+    hooks: {
+      // set up beforeCreate sets the value on the password beofre saving it.
+      // async function allows it to run simultaneously with other functions as a promise
+      async beforeCreate(newUser) {
+        // encrypting password. Salt rounds define the cost factor of the hashing.
+        newUser.password = await bcrypt.hash(newUser.password, 10);
+        return newUser;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
